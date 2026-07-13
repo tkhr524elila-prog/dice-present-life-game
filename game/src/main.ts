072fc51469db6c8a2b660f7a1446b7f5e7847d92
tkhr524elila-prog <tkhr524elila@gmail.com>
2026-07-14 00:02:32 +0900

    P1-01 ViteとTypeScriptの開発環境を追加

---
+++ game/src/main.ts
@@ -0,0 +1,8 @@
+import './style.css'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <main class="setup-check">
+    <p>開発環境の準備が完了しました</p>
+    <h1>「サイコロを振ってプレゼントが決まる、人生ゲーム的なゲーム」</h1>
+  </main>
+`
